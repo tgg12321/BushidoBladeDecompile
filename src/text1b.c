936cb9de393399c8c831f440611247cd6d79c768
Trenton <15005924+tgg12321@users.noreply.github.com>
2026-07-17 15:29:06 -0500

    Match: func_8006A494 — COMPLETED-C (grinder, 2 sessions)

--- src/text1b.c
+++ src/text1b.c
@@ -15650,9 +15650,7 @@ void func_8006A3CC(s32 *arg0, u8 *arg1) {
 }
 extern s32 func_80073728(s32, s32);
 void func_8006A494(s32 *arg0, u8 *arg1) {
-    s32 v;
     *(s32 *)(arg1 + 0) = *(s32 *)(*(s32 *)(*(s32 *)((u8 *)arg0 + 4) + 0x1C) + 0x24);
-    v = *(volatile s32 *)(arg1 + 0);
     *(s32 *)(arg1 + 0x18) = 0;
     *(s32 *)(arg1 + 0x1C) = 0;
     *(s8 *)(arg1 + 0x28) = 0;
@@ -15660,7 +15658,7 @@ void func_8006A494(s32 *arg0, u8 *arg1) {
     *(s32 *)(arg1 + 0x14) = 1;
     *(s32 *)(arg1 + 0x20) = 0x100;
     *(s32 *)(arg1 + 0x24) = 0x100;
-    *(s32 *)(arg1 + 4) = v + 0xC;
+    *(s32 *)(arg1 + 4) = *(s32 *)(arg1 + 0) + 0xC;
     *(s32 *)(arg1 + 0xC) = arg0[2];
     arg0[2] = func_80073728((s32)arg1, 0);
     initTexPage(arg0[7], 1, 0, saMotionSet(*(s32 *)(arg1 + 0), 0), 0);
