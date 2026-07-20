ecd8e623319341afa2f8efea652fcc53325ed6f8
Trenton <15005924+tgg12321@users.noreply.github.com>
2026-07-20 01:35:11 -0500

    Match: mario_getMarioVoiceData_8005BE84 — COMPLETED-C (grinder, 2 sessions)

--- src/text1b.c
+++ src/text1b.c
@@ -12525,7 +12525,7 @@ void func_8005BDF0(void) {
         s0++;
     } while ((s32)s0 < (s32)s1);
 }
-extern s32 D_8009AD1C;
+extern s16 D_8009AD1C[][2];
 extern s32 title_mv_exec2(s32);
 extern s32 func_80085F98();
 extern s32 func_80085EE4(s16);
@@ -12534,28 +12534,23 @@ extern s32 func_80085FB8();
 extern s32 md_game_check_change_main_mode_katinuki(s16);
 s32 mario_getMarioVoiceData_8005BE84(s32 arg0)
 {
-  register s32 arg_save asm("$16") = arg0;
-  s16 new_var;
   s32 result;
   s16 *p;
-  unsigned char new_var2;
+  s16 temp_a0;
+  s16 *base;
+  s32 doubled;
   title_mv_exec2(0);
-  new_var2 = 2;
+  base = &D_8009AD1C[0][0];
+  p = base + arg0 * 2;
+  doubled = arg0 << 1;
+  if (*p >= 0)
   {
-    register s32 base asm("$3");
-    base = (s32) (&D_8009AD1C);
-    p = (s16 *) (base + (new_var2 * (new_var2 * arg_save)));
-  }
-  if ((*p) >= 0)
-  {
-    s16 temp_a0;
-    arg_save = arg_save << 1;
     func_80085F98();
     func_80085EE4(0);
     func_80085E4C(0, 0);
     result = func_80085EE4(*p);
     md_game_check_change_main_mode_katinuki(*p);
-    temp_a0 = arg_save + 1;
+    temp_a0 = doubled + 1;
     func_80085E4C(temp_a0, temp_a0);
     func_80085FB8();
   }
@@ -12563,8 +12558,7 @@ s32 mario_getMarioVoiceData_8005BE84(s32 arg0)
   {
     result = -1;
   }
-  new_var = (s16) result;
-  return new_var;
+  return (s16) result;
 }
 void func_800858D0(s32);
 void func_80085F98(void);
